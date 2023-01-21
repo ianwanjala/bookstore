b24129228c6c778874e1190aed17322a2dbcbc1e
iano <ian.lukuya@strathmore.edu>
2023-01-21 17:31:25 +0300

    fancy view for bookdetail.html added

--- storecatalog/views.py
+++ storecatalog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from .models import Book
+from .models import Book, Genre, Publisher
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 
@@ -18,5 +18,7 @@ def index(request):
     return render(request, 'storecatalog/index.html', {'books':books})
 #// view function to return book details for selected book
 def bookdetail(request, book_id):
-    book = get_object_or_404(Book, pk=book_id)
-    return render(request, 'storecatalog/bookdetail.html',{'book':book})
+    book_detail = get_object_or_404(Book, pk=book_id)
+   # genre = get_object_or_404(Genre,fk=genre )
+    #publisher=get_object_or_404(Publisher,fk=publisher)
+    return render(request, 'storecatalog/bookdetail.html',{'book':book_detail})
